6ffc6fbf50a21255d079b3af2f891122a807a10b
cui <18530983738@163.com>
2017-04-12 08:59:45 +0800

    .

--- 9/socket_server.py
+++ 9/socket_server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
 import socketserver
 
 
@@ -21,5 +22,17 @@ class Myserver(socketserver.BaseRequestHandler):
 
 
 if __name__ == "__main__":
+    #socket+select+多线程
+    #ip和端口，类名
+    #Myserver==>RequestHandlerClass
+    #obj=self.RequestHandlerClass()
+    #obj.handle()  执行我们写的方法
+    #ThreadingTCPServer.init=>TCPServer.init()=>BaseTCPServer.init()
+    #1、创建server对象
+    #   self.server_address         ("127.0.0.1", 9999)
+    #   self.RequestHandlerClass    MyServer
+    #   self.socket                 创建服务端socket对象
     server = socketserver.ThreadingTCPServer(("127.0.0.1", 9999), Myserver)
+    # 2、server对象的server_forever()方法
+    #   在BaseServer中
     server.serve_forever()
