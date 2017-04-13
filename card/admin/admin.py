1278c1bd4aa015215a527d2da6d7d07ff739720f
cui <18530983738@163.com>
2017-04-13 19:23:06 +0800

    .

---
+++ card/admin/admin.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from lib import modules
+
+while True:
+    inp = input("管理员1、注册。2、登陆\n>>>")
+    if inp =="1":
+        username = input("用户名：")
+
---
+++ card/admin/lib/modules.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+class Admin:
+    def __init__(self,*args):
+        self.username = args[0]
+        self.password = args[1]
+
+    def Register(self):
+        pass
+    def Login(self):
+        pass
+    def Open_card(self):
+        pass
+    def Close_card(self):
+        pass
