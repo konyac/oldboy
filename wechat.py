3f03015b710bcafd61f3af5dcf767f3023605893
cui <18530983738@163.com>
2017-06-07 18:05:47 +0800

    .

--- wechat.py
+++ wechat.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import itchat
+from itchat.content import TEXT
 
-itchat.login()
-itchat.send("你好","filehelper")
+
+
+itchat.auto_login()
+list=itchat.get_friends()
+memberList = itchat.update_chatroom('翰林')
+print(list)
+
+print(memberList)
+# itchat.send_msg(list)
