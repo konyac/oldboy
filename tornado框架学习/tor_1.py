772499adc8a08a8e8a08ab991ab0c47c29cf4d02
cui <18530983738@163.com>
2017-07-05 18:00:05 +0800

    .

--- tornado框架学习/tor_1.py
+++ tornado框架学习/tor_1.py
@@ -3,17 +3,34 @@
 import tornado.web
 import tornado.ioloop
 
-class Mainhandler(tornado.web.RequestHandler):#继承类RequestHandler
+INPUTS_LIST = []
+
+
+class Mainhandler(tornado.web.RequestHandler):  # 继承类RequestHandler
     def get(self):
         # self.write("hello world")
-        self.render("s2.html")
+        # 1、打开s1.html文件，读取内容，（包含特殊语法）
+        # 2、把这个字符串进行一个分解。将传入的值与html进行结合渲染。
+        # 3、渲染完毕得到新的字符串，得到已经替换完毕的字符串。
+        # 4、返回给用户。
+        self.render("s1.html", inputs=INPUTS_LIST)
+
+    def post(self, *args, **kwargs):
+        name = self.get_argument("ooxx")#获取用户提交的数据
+        print(name)
+        INPUTS_LIST.append(name)
+        self.render("s1.html",inputs=INPUTS_LIST)
+
+
+#     get方式 url中传输数据。
+
 
 settings = {
-    "template_path":"template",#模板路径的配置
-    "static_path":"static",#静态文件的位置
+    "template_path": "template",  # 模板路径的配置
+    "static_path": "static",  # 静态文件的位置
 }
-#路由映射，路由系统
-application = tornado.web.Application([(r"/index",Mainhandler)],**settings)#创建一个对象
+# 路由映射，路由系统
+application = tornado.web.Application([(r"/index", Mainhandler)], **settings)  # 创建一个对象
 
 if __name__ == "__main__":
     application.listen(8000)
