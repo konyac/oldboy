e3eea9bc4f6596e64aac99f5b0c46ebc8a6c9562
cui <18530983738@163.com>
2017-07-11 19:06:43 +0800

    .

--- cookie/index.py
+++ cookie/index.py
@@ -2,6 +2,7 @@
 # _*_ coding:utf-8 _*_
 import tornado.web
 import tornado.ioloop
+import time
 
 
 class IndexHandler(tornado.web.RequestHandler):
@@ -14,16 +15,19 @@ class IndexHandler(tornado.web.RequestHandler):
 
 class LoginHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
-        self.render("login.html",status_text = "")
+        self.render("login.html", status_text="")
 
     def post(self, *args, **kwargs):
         username = self.get_argument("username")
         password = self.get_argument("pwd")
         if username == "alex" and password == "123":
-            self.set_cookie("auth", "1")
+            r = time.time()  # 获取当前时间戳,
+            r = r + 10  # 使时间戳在当前时间下+10秒。
+            # self.set_cookie("auth","1")设置普通cookie。
+            self.set_secure_cookie("auth", "1", expires=r)  # expires 过期时间。
             self.redirect("/manager")
         else:
-            self.render("login.html",status_text = "登陆失败")
+            self.render("login.html", status_text="登陆失败")
 
 
 class LogoutHandler(tornado.web.RequestHandler):
@@ -45,7 +49,8 @@ class ManagerHandler(tornado.web.RequestHandler):
 
 
 settings = {
-    "template_path": "views"
+    "template_path": "views",
+    "cookie_secret": "alex"
 }
 
 app = tornado.web.Application([("/index", IndexHandler),
