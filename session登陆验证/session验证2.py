1b2d2f8398c05fca6660744afb430f2b901a4db5
cui <18530983738@163.com>
2017-08-10 08:45:26 +0800

    .

--- session登陆验证/session验证2.py
+++ session登陆验证/session验证2.py
@@ -26,21 +26,21 @@ class Session:
         # 在客户端中写入随机字符串
         # 判断，请求的用户是否已有随机字符串
         # random_str = self.handler.get_cookie("__kakaka__")
-        if not self.random_str:
+        if not self.random_str: #将每次的写入值，写到同一个key中，即同一个随机字符串中
             random_str = self.handler.get_cookie("__kakaka__")
-            if not random_str:
+            if not random_str:#第一次登陆，客户端没有cookie，设置cookie。
                 random_str = self.__genarate_random_str()
                 # self.random_str = random_str
                 container[random_str] = {}
-            else:
-                if random_str in container.keys():#container中已经有这个随机字符串了
+            else:#不是第一次登陆，客户端有cookie
+                if random_str in container.keys():#客户端的中的cookie在服务器container中，已经有这个随机字符串了
                     pass
-                else:
+                else:#客户端的cookie不是服务器container中的。
                     random_str = self.__genarate_random_str()
                     container[random_str] = {}
-            self.random_str = random_str
-        container[self.random_str][key] = value
-        self.handler.set_cookie("__kakaka__", self.random_str)
+            self.random_str = random_str#重新给self.random_str 负责
+        container[self.random_str][key] = value #设置key value
+        self.handler.set_cookie("__kakaka__", self.random_str) #写入cookie。设置完成
 
     def get_value(self, key):
         # 获取客户端的随机字符串
